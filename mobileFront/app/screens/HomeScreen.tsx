ae13e4cba44e217979eb337dfd21f3b2efce8c87
kahtab1324 <k.s.fayyad.dev@gmail.com>
2025-07-17 02:48:06 +0300

    commit

--- mobileFront/app/screens/HomeScreen.tsx
+++ mobileFront/app/screens/HomeScreen.tsx
@@ -3,6 +3,6 @@ import { Text } from "react-native";
 
 export const HomeScreen = () => {
   const leen = "hello there i am leen from my brach2";
-  const leen = "hello there i am leen from my brach2";
+  const sllhe = "helldo there i am leen from my brach2";
   return <Text>hello</Text>;
 };
