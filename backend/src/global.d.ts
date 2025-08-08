f6b2803737d88741cbc5f4c447e9f264432e42a7
leen <programmerleen@gmail.com>
2025-08-07 19:37:05 -0700

    edit

---
+++ backend/src/global.d.ts
@@ -0,0 +1,6 @@
+declare module 'fastify-type-provider-zod';
+declare module '@fastify/jwt';
+declare module '@fastify/formbody';
+declare module '@fastify/multipart';
+declare module 'qs';
+declare module 'dotenv';
